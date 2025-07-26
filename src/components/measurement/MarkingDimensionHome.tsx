e629644ae92464af7c140ae304fe4d50b0a28546
vandiabloai <dzinlydev@gmail.com>
2025-07-26 16:17:06 +0530

    Added OldDimension.tsx

---
+++ src/components/measurement/MarkingDimensionHome.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import StartMarkingDimension from "./StartMarkingDimension";
+import OldDimension from "./OldDimension";
+import { useSelector } from "react-redux";
+import { getSelectedSegment } from "../../../../slice/userJobSlice/UserJobSlice";
+
+const MarkingDimensionHome = () => {
+  const [isDimensionStarted, setIsDimensionStarted] = useState(false);
+  const getSelectedSegments = useSelector(getSelectedSegment);
+
+  useEffect(() => {
+    if (
+      getSelectedSegments &&
+      getSelectedSegments.length > 0 &&
+      getSelectedSegments[0].details &&
+      getSelectedSegments[0].details.area !== undefined &&
+      getSelectedSegments[0].details.area !== null
+    ) {
+      setIsDimensionStarted(true);
+    } else {
+      setIsDimensionStarted(false);
+    }
+  }, [getSelectedSegments]);
+
+  return (
+    <>
+      {!isDimensionStarted ? (
+        <StartMarkingDimension />
+      ) : (
+        <OldDimension
+          restartDimensionRef={() => setIsDimensionStarted(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default MarkingDimensionHome;
